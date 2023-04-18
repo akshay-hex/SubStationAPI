eee057c1ae7861ccc94f46bacb424fa81c97a48f
INGRNET\akumar <akshay.kumar.1@hexagon.com>
2023-04-18 10:55:44 +0530

    Add project files.

---
+++ SubStationAPI/Controllers/SubstationController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SubStationAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]/[action]")]
+    public class SubstationController : ControllerBase
+    {
+
+        [HttpGet(Name = "GetCurrentLoad")]
+        public double GetCurrentLoad()
+        {
+            double maximum = 3.0;
+            double minimum = 1.5;
+            Random random = new Random();
+            return random.NextDouble() * (maximum - minimum) + minimum;
+        }
+    }
+}
